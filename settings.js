e773aed9a5001addce27a6e7ed5e7749bc1bac6b
Jiho Yoo <ssy05468@naver.com>
2021-02-09 10:36:34 +0900

    add set frequency

--- settings.js
+++ settings.js
@@ -3,9 +3,12 @@ var frequency = 0;
 document.getElementById('set').addEventListener('click', setFreq, false);
 
 function setFreq(){
-    console.log("debug");
+    chrome.storage.local.get('frequency', (response) => {
     var F = document.getElementById("frequency");
-    frequency = F.options[F.selectedIndex].value;    
+    frequency = F.options[F.selectedIndex].value;
+    chrome.storage.local.set({ frequency });    
+    alert(frequency);
+    })
 }
 
 /*
